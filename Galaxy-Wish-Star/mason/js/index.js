4eaa07eba8267f505caf9c22cf576ef5f8c89da6
mason369 <maosenyang00@gmail.com>
2022-09-14 15:57:33 +0800

    Update index.js

--- Galaxy-Wish-Star/mason/js/index.js
+++ Galaxy-Wish-Star/mason/js/index.js
@@ -1,9 +1,15 @@
-window.addEventListener("load", function(){
-    let swipercontainer=document.querySelector("#swiper-container");
-    let swiperslide=document.querySelector("#swiper-swiper");
+window.addEventListener("load", function () {
+    let swipercontainer = document.querySelector("#swiper-container");
+    let swiperslide = document.querySelector("#swiper-swiper");
+    let a
+    swiperslide.style.transition = 'all 1.2s ease'
+    a="'"+"translateY("+"-"+b+"px)'"
+    let b
+    for (let i = 0; i >= 3; i++) {
+        setTimeout(() => {
+            swiperslide.style.transform = a
 
-setTimeout(() => {
-    swiperslide.style.transform='translateY(-420px)'
-    swiperslide.style.transition='all 1.2s ease'
-}, 1000);
+        }, 1000);
+        b*=2;
+    }
 });
