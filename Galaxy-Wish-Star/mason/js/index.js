749da2e5f373501631a7005e75722ed168340a23
mason369 <maosenyang00@gmail.com>
2022-09-14 18:53:36 +0800

    update

--- Galaxy-Wish-Star/mason/js/index.js
+++ Galaxy-Wish-Star/mason/js/index.js
@@ -1,10 +1,20 @@
 window.addEventListener("load", function () {
     let swipercontainer = document.querySelector(".swiper-container");
     let swiperslide = document.querySelectorAll('.swiper-slide');
-
-    swiperslide[0].classList.add('swiper-slide')
-    swiperslide[1].classList.add('swiper-slide')
-    swiperslide[2].classList.add('swiper-slide2')
+setTimeout(() => {
+    swiperslide[0].classList.add('swiper-slide1')
+    swiperslide[1].classList.add('swiper-slide1')
+}, 2500);
+    setTimeout(() => {
+        swiperslide[1].classList.remove('swiper-slide1')
+        swiperslide[1].classList.add('swiper-slide2')
+        swiperslide[2].classList.add('swiper-slide2')
+    }, 5000);
+   setTimeout(() => {
+       swiperslide[2].classList.remove('swiper-slide2')
+    swiperslide[2].classList.add('swiper-slide3')
     swiperslide[3].classList.add('swiper-slide3')
-    console.log(swiperslide)
+   }, 7500);
+    
+
 });
