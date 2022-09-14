2925e050ebc7eff8d7e2708b8fb93ddf32f877f1
mason369 <maosenyang00@gmail.com>
2022-09-14 17:48:41 +0800

    upadte

--- Galaxy-Wish-Star/mason/js/index.js
+++ Galaxy-Wish-Star/mason/js/index.js
@@ -1,9 +1,10 @@
 window.addEventListener("load", function () {
-    let swipercontainer = document.querySelector("#swiper-container");
-    let swiperslide = document.querySelector("#swiper-swiper");
+    let swipercontainer = document.querySelector(".swiper-container");
+    let swiperslide = document.querySelectorAll('.swiper-slide');
 
-
-    for (let i = 0; i<=3; i++) {
-        swiperslide.classList.add('swiper-swiper1')
-    }
+    swiperslide[0].classList.add('swiper-slide')
+    swiperslide[1].classList.add('swiper-slide')
+    swiperslide[2].classList.add('swiper-slide2')
+    swiperslide[3].classList.add('swiper-slide3')
+    console.log(swiperslide)
 });
